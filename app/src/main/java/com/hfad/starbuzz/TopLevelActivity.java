5c07547363064c765f9cd803df41c5600f692429
Ruslan Sabirov <ruslan.sabirov@russianpost.ru>
2019-01-29 09:03:37 +0300

    commit 4

--- app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -23,6 +23,7 @@ public class TopLevelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
         setupOptionsListView();
         setupFavoritesListView();
     }
